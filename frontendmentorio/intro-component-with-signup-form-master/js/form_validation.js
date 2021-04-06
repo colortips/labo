024e37292e3f5b573fab54a809930d57af7df6cb
colortips <h.schoen@intech-so.fr>
2021-04-06 23:24:25 +0200

    Finished implementation of form validation

--- frontendmentorio/intro-component-with-signup-form-master/js/form_validation.js
+++ frontendmentorio/intro-component-with-signup-form-master/js/form_validation.js
@@ -19,6 +19,7 @@ const PassIconErr = document.getElementById('err-icon-PW');
 const okButton = document.getElementById('Send-Form');
 
 signUpForm.addEventListener('submit', (e) => {
+    // Firstname field ---------------------------------------------------
     let messages_FN = []
     if (FnField.value === '' || FnField.value == null){
         messages_FN.push("First Name cannot be empty")
@@ -29,8 +30,15 @@ signUpForm.addEventListener('submit', (e) => {
         FnField.style.border = "1px solid hsl(0, 100%, 74%)"
         FnIconErr.style.height = "auto"
         FnIconErr.style.visibility = "visible"
+    }else{
+        e.preventDefault()
+        FnFieldErr.innerText =  messages_FN.join(', ')
+        FnField.style.border = "none"
+        FnIconErr.style.height = "0"
+        FnIconErr.style.visibility = "hiden"
     }
 
+    // Lastname field ---------------------------------------------------
     let messages_LN = []
     if (LnField.value === '' || LnField.value == null){
         messages_LN.push("Last Name cannot be empty")
@@ -41,10 +49,20 @@ signUpForm.addEventListener('submit', (e) => {
         LnField.style.border = "1px solid hsl(0, 100%, 74%)"
         LnIconErr.style.height = "auto"
         LnIconErr.style.visibility = "visible"
+    }else{
+        e.preventDefault()
+        LnFieldErr.innerText =  messages_LN.join(', ')
+        LnField.style.border = "none"
+        LnIconErr.style.height = "0"
+        LnIconErr.style.visibility = "hiden"
     }
+
+    // Email field ---------------------------------------------------
     let messages_Email = []
     if (emailField.value === '' || emailField.value == null){
-        messages_Email.push("Password cannot be empty")
+        messages_Email.push("Email cannot be empty")
+    }else if (!validateEmail(emailField.value)){
+        messages_Email.push("Looks like this is not an email")
     }
     if (messages_Email.length > 0){
         e.preventDefault()
@@ -52,19 +70,19 @@ signUpForm.addEventListener('submit', (e) => {
         emailField.style.border = "1px solid hsl(0, 100%, 74%)"
         emailIconErr.style.height = "auto"
         emailIconErr.style.visibility = "visible"
+    }else{
+        e.preventDefault()
+        emailFieldErr.innerText =  messages_Email.join(', ')
+        emailField.style.border = "none"
+        emailIconErr.style.height = "0"
+        emailIconErr.style.visibility = "hiden"
     }
 
+    // Password field ---------------------------------------------------
     let messages_Pass = []
-    var paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
-    if(inputtxt.match(paswd)){
-        return true;
-    }
-    else{ 
-        messages_Pass.push("A password which contain at least one numeric digit and a special character is needed");
-        return false;
-    }
-    
-    if (CheckPassword(PassField.value)){
+    if (PassField.value === '' || PassField.value == null){
+        messages_Pass.push("Password cannot be empty")
+    }else if (!CheckPassword(PassField.value)){
         messages_Pass.push("A password which contain at least one numeric digit and a special character is needed")
     }
     if (messages_Pass.length > 0){
@@ -73,18 +91,28 @@ signUpForm.addEventListener('submit', (e) => {
         PassField.style.border = "1px solid hsl(0, 100%, 74%)"
         PassIconErr.style.height = "auto"
         PassIconErr.style.visibility = "visible"
+    }else{
+        e.preventDefault()
+        PassFieldErr.innerText =  messages_Pass.join(', ')
+        PassField.style.border = "none"
+        PassIconErr.style.height = "0"
+        PassIconErr.style.visibility = "hiden"
     }
 })
 
 function CheckPassword(inputtxt) 
 { 
     var paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
-    if(inputtxt.match(paswd)){
-        alert('Correct, try another...') 
+    console.log(inputtxt + ': ' + paswd.test(inputtxt));
+    if(paswd.test(inputtxt)){
         return true;
     }
     else{ 
-        messages_Pass.push("A password which contain at least one numeric digit and a special character is needed")
         return false;
     }
 }
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
